081af101bdef583b14d1581376b2eefd4bf133d4
SchneiderMan <jschneid94@gmail.com>
2018-08-22 16:45:14 -0700

    Created addProduct function.

--- bamazonManager.js
+++ bamazonManager.js
@@ -21,7 +21,7 @@ function startMenu() {
             name: "startMenu",
             type: "list",
             message: "Bamazon Manager",
-            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
+            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product", "Exit"]
         }
     ]).then(function(response){
         if (response.startMenu === "View Products for Sale") {
@@ -30,8 +30,10 @@ function startMenu() {
             lowInventory();
         } else if (response.startMenu === "Add to Inventory") {
             addInventory();
-        } else {
+        } else if (response.startMenu === "Add New Product") {
             addProduct();
+        } else {
+            connection.end();
         }
     });
 }
@@ -58,12 +60,12 @@ function addInventory() {
     var sqlString = "SELECT * FROM products";
     connection.query(sqlString, function(err, res) {
         if (err) throw err;
-
+        // Create an array of all products for choice options
         var choiceArray = [];
         for (var i = 0; i < res.length; i++) {
             choiceArray.push(res[i].item_id + ": " + res[i].product_name);
         }
-
+        // Prompt for the item to increase inventory for and the amount to increase
         var questions = [
             {
                 name: "choice",
@@ -83,8 +85,8 @@ function addInventory() {
                 } 
             }
         ];
-
         inquirer.prompt(questions).then(function(answer) {
+            // Assigns the choice components to a variable
             var chosenItem;
             var components = answer.choice.trim().split(":");
             for (var i = 0; i < res.length; i++) {
@@ -92,9 +94,8 @@ function addInventory() {
                     chosenItem = res[i];
                 }
             }
-
+            // Update the chosen item's stock quantity
             var newQuantity = parseInt(chosenItem.stock_quantity) + parseInt(answer.quantity);
-
             var quantityQuery = "UPDATE products SET ? WHERE ?";
             var values = [
                 {
@@ -104,7 +105,7 @@ function addInventory() {
                     item_id: chosenItem.item_id
                 }
             ]
-
+            // Notify the user of the item and its updated quantity
             connection.query(quantityQuery, values, function(err) {
                 if (err) throw err;
                 console.log(`\nInventory updated: ${chosenItem.product_name} - ${newQuantity}\n`);
@@ -115,18 +116,69 @@ function addInventory() {
 }
 
 function addProduct() {
-
-}
-
-function exit() {
-    inquirer.prompt([
+    var questions = [
         {
-            name: "exit",
-            type: "list",
-            message: "",
-            choices: ["EXIT"]
+            name: "name",
+            type: "input",
+            message: "What is the name of the product you would like to add?"
+        },
+        {
+            name: "department",
+            type: "input",
+            message: "What department does this product belong in?"
+        },
+        {
+            name: "price",
+            type: "input",
+            message: "What is the price for this item?",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            } 
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "How many of these items do you want in stock?",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
         }
-    ]).then(function() {
-        startMenu();
-    });
+    ];
+    inquirer.prompt(questions).then(function(res) {
+        console.table(res);
+        // Confirm the entered information is correct
+        var confirm = [
+            {
+                name: "confirm",
+                type: "list",
+                message: "Does this information look correct?",
+                choices: ["YES", "NO"]
+            }
+        ];
+        inquirer.prompt(confirm).then(function(answer) {
+            if (answer.confirm === "YES") {
+                var sqlString = "INSERT INTO products SET ?";
+                var newProduct = {
+                    product_name: res.name,
+                    department_name: res.department,
+                    price: res.price,
+                    stock_quantity: res.quantity
+                };
+                connection.query(sqlString, newProduct, function(err) {
+                    if (err) throw err;
+                    viewProducts();
+                    console.log("Product has been added!")
+                })
+            } else {
+                console.log(`\nPlease re-enter the product info\n`)
+                addProduct();
+            }
+        });
+    })
 }
