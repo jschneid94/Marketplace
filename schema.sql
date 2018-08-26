2b138d5cec4238164cb4f60b98f2c6627dd43a78
SchneiderMan <jschneid94@gmail.com>
2018-08-25 20:35:23 -0700

    Created schema and seeds sql files.

--- schema.sql
+++ schema.sql
@@ -15,25 +15,9 @@ CREATE TABLE departments (
     department_id INT NOT NULL AUTO_INCREMENT,
     department_name VARCHAR(50) NOT NULL,
     over_head_costs DECIMAL(15,2) NOT NULL,
+    product_sales DECIMAL(15,2) DEFAULT 0,
     PRIMARY KEY (department_id)
 );
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Speaker", "Electronics", 49.95, 500), 
-    ("Towel Set", "Bathroom", 15, 150),
-    ("Firestick", "Electronics", 39.99, 1000),
-    ("Mixer", "Kitchen", 79.99, 100);
-
-INSERT INTO departments (department_name, over_head_costs)
-Values ("Home & Furniture", 300),
-        ("Kitchen", 400),
-        ("Electronics", 1000),
-        ("Bathroom", 200),
-        ("Outdoor", 50),
-        ("Beauty", 80);
-
-ALTER TABLE products 
-    ADD product_sales DECIMAL(15,2) DEFAULT 0;
 
 CREATE VIEW product_sales_by_department AS
 SELECT department_id AS ID, 
---
+++ seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Speaker", "Electronics", 49.95, 5), 
+    ("Towel Set", "Bathroom", 15, 10),
+    ("Firestick", "Electronics", 39.99, 20),
+    ("Mixer", "Kitchen", 79.99, 12),
+    ("Rug", "Home & Furniture", 34.99, 8),
+    ("Laptop", "Electronics", 600, 15),
+    ("Waterbottle", "Outdoor", 8.95, 9),
+    ("Hairties", "Beauty", 1.00, 18),
+    ("Soap", "Bathroom", 3.00, 6),
+    ("Stool", "Home & Furniture", 21.99, 6);
+
+INSERT INTO departments (department_name, over_head_costs)
+Values ("Home & Furniture", 300),
+        ("Kitchen", 400),
+        ("Electronics", 1000),
+        ("Bathroom", 200),
+        ("Outdoor", 50),
+        ("Beauty", 80);
